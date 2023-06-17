a06671f171e18bdda2d4e28a7a488d1403b6db80
Niranjana <DevNiranjana@gmail.com>
2023-06-17 18:12:32 +0530

    jdbcTemplate

---
+++ SpBootJdbcTemplateCallbackInterface_RowCallbackHandler/src/main/java/com/eidiko/niranjana/SpBootJdbcTemplateCallbackInterfaceRowCallbackHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.eidiko.niranjana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBootJdbcTemplateCallbackInterfaceRowCallbackHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpBootJdbcTemplateCallbackInterfaceRowCallbackHandlerApplication.class, args);
+	}
+
+}
